a96eaebee6cbf6b926369fd259412a871a7acf6d
Alex Recker <alex@reckerfamily.com>
2015-10-29 18:43:12 -0500

    Fetch data for homepage

--- showing/views.py
+++ showing/views.py
@@ -1,10 +1,15 @@
 from django.views.generic import DetailView, TemplateView
 
-from models import Gallery
+from models import Gallery, Item
 
 
 class Index(TemplateView):
     template_name = 'showing/index.html'
+
+    def get_context_data(self, *args, **kwargs):
+        context = super(Index, self).get_context_data(*args, **kwargs)
+        context['carousel'] = Item.objects.in_carousel()
+        return context
 
 
 class GalleryDetail(DetailView):
