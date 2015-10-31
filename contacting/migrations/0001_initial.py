1f3800500333027bc17d852bf3c7251eae951f87
Alex Recker <alex@reckerfamily.com>
2015-10-31 15:37:56 -0500

    Squashes migrations

--- contacting/migrations/0001_initial.py
+++ contacting/migrations/0001_initial.py
@@ -16,7 +16,9 @@ class Migration(migrations.Migration):
             fields=[
                 ('id', models.UUIDField(primary_key=True, default=uuid.uuid4, serialize=False, editable=False, unique=True)),
                 ('timestamp', models.DateTimeField(auto_now_add=True)),
+                ('address', models.GenericIPAddressField(null=True)),
                 ('email', models.EmailField(max_length=254)),
+                ('subject', models.CharField(max_length=120)),
                 ('message', models.TextField()),
             ],
             options={
